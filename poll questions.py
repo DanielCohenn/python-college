e5530e0492fdba00b30aeadf7bb80575b97946ba
MrDaniCo <69191953+MrDaniCo@users.noreply.github.com>
2020-08-04 13:29:28 +0300

    Update poll questions.py

--- poll questions.py
+++ poll questions.py
@@ -3,6 +3,7 @@ class Poll:
     def __init__(self, poll_q, vote_option_list):
         self.poll_q = poll_q
         self.vote_option_list = vote_option_list
+        # generating a dictionary with the options to vote (keys) and reset the number of votes to zero
         count_list = []
         for n in range(len(self.vote_option_list)):
             count_list.append(0)
@@ -10,19 +11,23 @@ class Poll:
         print(f'New poll question: {self.poll_q}, vote options: {vote_option_list}')
 
     def vote(self, vote):
+        # updating our vote dictionary according to the inputted votes
         for n in self.votes_dict:
             if vote == n:
                 self.votes_dict[n] += 1
 
     def add_option(self, option):
+        # adds a vote option
         self.vote_option_list.append(option)
         return self.vote_option_list
 
     def remove_option(self, option):
+        # removes a vote option
         self.vote_option_list.append(option)
         return self.vote_option_list
 
     def get_votes(self):
+        # giving us the current situation of the poll votes
         return self.votes_dict
 
     def get_winner(self):
@@ -34,6 +39,7 @@ class Poll:
 
 
 def cast_multiple_votes(poll, votes):
+    # pushing votes one by one to our vote function
     for vote in votes:
         poll.vote(vote)
 
