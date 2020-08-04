a596e4d24fc43bf6825ee79ce1ae7c10a7b761ff
MrDaniCo <dani1998c@gmail.com>
2020-08-04 21:39:03 +0300

    plotting data from xl with (x,y) values and value L that tells the color of the (x,y) values

--- ploting data from XL.py
+++ ploting data from XL.py
@@ -2,16 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas
 
+
 def import_data(file):
     arr1 = np.array(file[['L']])
-    arr2 = np.array(file[['X','Y']])
+    arr2 = np.array(file[['X', 'Y']])
     arr3 = np.array([file['NewX'], file['NewY']])
     return arr1, arr2, arr3
 
 
 def plot_my_data(arr1, arr2, arr3):
     color_dict = {0: 'red', 1: 'green', 2: 'blue'}
-    #lets split arr2 to two different arrays with x values and y values
+    # lets split arr2 to two different arrays with x values and y values
     split_X = np.array(file['X'])
     split_Y = np.array(file['Y'])
     for i, val in enumerate(arr1):
@@ -22,6 +23,7 @@ def plot_my_data(arr1, arr2, arr3):
         else:
             plt.scatter(split_X[i], split_Y[i], color=color_dict[2], edgecolors='black')
     plt.show()
+
 
 file = pandas.read_excel('Data1.xlsx')
 arr1, arr2, arr3 = import_data(file)
