d09f9d59c9c1bdf7d7c1be637be0351bd5af6f87
MrDaniCo <dani1998c@gmail.com>
2020-08-17 18:39:51 +0300

    project to plot xl data

--- ploting_data_from_xl.py
+++ ploting_data_from_xl.py
@@ -6,7 +6,8 @@ import pandas
 def import_data(file):
     """
     this function let us import our desired data from an xl file,
-    than we split our data into 3 different arrays
+    than we split our data into 3 different arrays.
+    arr1 --> L ; arr2 --> NewX ; arr3 --> NewY
     """
     arr1 = np.array(file[['L']])
     arr2= np.array([file['NewX']])
@@ -16,12 +17,15 @@ def import_data(file):
 
 def plot_my_data(arr1: list, arr2: list, arr3: list):
     """
-    plotting our according to the values in arr1, each value gives different color.
+    plotting our data in respect to the values in arr1, each value gives different color.
     """
     color_dict = {0: 'red', 1: 'green', 2: 'blue'}
-    # lets split arr2 to two different arrays with x values and y values
+    # extracting X and Y data from file
     split_X = np.array(file['X'])
     split_Y = np.array(file['Y'])
+    f = plt.figure(figsize=(8, 8))
+    # plotting our X and Y data
+    plt.subplot(2, 1, 1)
     for i, val in enumerate(arr1):
         # i represents the index in arr1 and val is the value corresponding to i
         if val == 0:
@@ -31,7 +35,25 @@ def plot_my_data(arr1: list, arr2: list, arr3: list):
             plt.scatter(split_X[i], split_Y[i], color=color_dict[1], edgecolors='black')
         else:
             plt.scatter(split_X[i], split_Y[i], color=color_dict[2], edgecolors='black')
-    #plt.show()
+    plt.title('X and Y scatter')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    # plotting our New x and New Y data
+    plt.subplot(2, 1, 2)
+    for i, val in enumerate(arr1):
+        # i represents the index in arr1 and val is the value corresponding to i
+        if val == 0:
+            plt.scatter(arr2[0][i], arr3[0][i], color=color_dict[0], edgecolors='black')
+
+        elif val == 1:
+            plt.scatter(arr2[0][i], arr3[0][i], color=color_dict[1], edgecolors='black')
+        else:
+            plt.scatter(arr2[0][i], arr3[0][i], color=color_dict[2], edgecolors='black')
+    plt.title('New X and New Y scatter')
+    plt.xlabel('New X')
+    plt.ylabel('New Y')
+    plt.tight_layout(pad=3.0)
+    plt.show()
 
 # reading our xl file with the panda.read built-in
 file = pandas.read_excel('Data1.xlsx')
