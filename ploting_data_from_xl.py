0f7720983c8aa4937f18944830812cb92b5f097c
MrDaniCo <dani1998c@gmail.com>
2020-08-17 12:29:35 +0300

    project to plot xl data

--- ploting_data_from_xl.py
+++ ploting_data_from_xl.py
@@ -4,27 +4,36 @@ import pandas
 
 
 def import_data(file):
+    """
+    this function let us import our desired data from an xl file,
+    than we split our data into 3 different arrays
+    """
     arr1 = np.array(file[['L']])
-    arr2 = np.array(file[['X', 'Y']])
-    arr3 = np.array([file['NewX'], file['NewY']])
+    arr2= np.array([file['NewX']])
+    arr3 = np.array([file['NewY']])
     return arr1, arr2, arr3
 
 
-def plot_my_data(arr1, arr2, arr3):
+def plot_my_data(arr1: list, arr2: list, arr3: list):
+    """
+    plotting our according to the values in arr1, each value gives different color.
+    """
     color_dict = {0: 'red', 1: 'green', 2: 'blue'}
     # lets split arr2 to two different arrays with x values and y values
     split_X = np.array(file['X'])
     split_Y = np.array(file['Y'])
     for i, val in enumerate(arr1):
+        # i represents the index in arr1 and val is the value corresponding to i
         if val == 0:
             plt.scatter(split_X[i], split_Y[i], color=color_dict[0], edgecolors='black')
+
         elif val == 1:
             plt.scatter(split_X[i], split_Y[i], color=color_dict[1], edgecolors='black')
         else:
             plt.scatter(split_X[i], split_Y[i], color=color_dict[2], edgecolors='black')
-    plt.show()
+    #plt.show()
 
-
+# reading our xl file with the panda.read built-in
 file = pandas.read_excel('Data1.xlsx')
 arr1, arr2, arr3 = import_data(file)
 plot_my_data(arr1, arr2, arr3)
